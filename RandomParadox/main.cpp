d1592199b9381c9c5c529b8ae855ce37105474a5
panik4 <25900219+panik4@users.noreply.github.com>
2022-01-05 18:38:27 +0100

    Make loading of heightmaps configurable in new config file, which also allows to enable or disable modules for generation and their paths

--- RandomParadox/main.cpp
+++ RandomParadox/main.cpp
@@ -3,11 +3,38 @@
 #include "generic/ScenarioGenerator.h"
 #include "hoi4/Hoi4Module.h"
 using namespace std;
+
+
+void readConfig() {
+
+}
+
+
 int main() {
+	// Read the basic settings
+	ifstream f("basic_settings.json");
+	std::stringstream buffer;
+	if (!f.good())
+	{
+		std::cout << "Config could not be loaded" << std::endl;
+	}
+	buffer << f.rdbuf();
+	// Short alias for this namespace
+	namespace pt = boost::property_tree;
+	// Create a root
+	pt::ptree root;
+	pt::read_json(buffer, root);
+
+	bool genHoi4Scenario = root.get<bool>("randomScenario.genhoi4");
+	// use the same input heightmap for every scenario/map generation
+	bool useGlobalExistingHeightmap = root.get<bool>("randomScenario.inputheightmap");
+	// get the path
+	std::string globalHeightMapPath = root.get<std::string>("randomScenario.heightmapPath");
+
 	bool useDefaultMap = false;
 	bool useDefaultStates = false;
 	bool useDefaultProvinces = false;
-	
+
 	bool genHoi4 = true;
 
 	if (!Data::getInstance().getConfig("config.json"))
@@ -15,17 +42,24 @@ int main() {
 		system("pause");
 		return -1;
 	}
-	FastWorldGenerator f;
+	FastWorldGenerator fastWorldGen;
 	Hoi4Module hoi4Mod;
 	if (!useDefaultMap)
 	{
-		f.generateWorld();
+		if (useGlobalExistingHeightmap)
+		{
+			// overwrite settings of fastworldgen
+			Data::getInstance().heightmapIn = globalHeightMapPath;
+			Data::getInstance().genHeight = false;
+		}
+		fastWorldGen.generateWorld();
 	}
-	ScenarioGenerator sG(f);
+	ScenarioGenerator sG(fastWorldGen);
 	if (genHoi4)
 	{
 		hoi4Mod.genHoi(useDefaultMap, useDefaultStates, useDefaultProvinces, sG);
 	}
 	return 0;
-	
+
 }
+
