0520196d60c67d6cc3d8edeacc988aa8300f1a29
pkramp <paul-niklas-kramp@web.de>
2022-02-12 17:32:21 +0100

    Refactoring of Hoi4Parser

--- RandomParadox/generic/countries/Country.h
+++ RandomParadox/generic/countries/Country.h
@@ -7,32 +7,33 @@
 #include "../../hoi4/NationalFocus.h"
 class Country
 {
-	
 public:
+	// member variables
 	std::string tag;
 	int ID;
+	std::string name;
+	std::string adjective;
 	int capitalRegionID;
+	double developmentFactor;
+	Flag flag;
+	Colour colour;
+	// constructors/destructors
 	Country();
 	Country(std::string tag);
 	~Country();
-	std::vector<GameRegion> ownedRegions;
-	std::set<std::string> neighbours;
-
-	std::string name;
-	std::string adjective;
-	double developmentFactor;
+	// containers
 	std::map<std::string, std::string> attributeStrings;
 	std::map<std::string, int> attributeDoubles;
 	std::map<std::string, vector<int>> attributeVectors;
-	Flag flag;
-	Colour colour;
+	std::vector<GameRegion> ownedRegions;
+	std::vector<std::vector<NationalFocus>> foci;
+	std::set<std::string> neighbours;
+	// member functions
 	void addRegion(GameRegion& region, vector<GameRegion>& gameRegions, vector<GameProvince>& gameProvinces);
 	void assignRegions(int maxRegions, vector<GameRegion>& gameRegions, GameRegion& startRegion, vector<GameProvince>& gameProvinces);
+	// operators
 	bool operator<(const Country& right) const	{
 		return ID < right.ID;
 	};
-
-
-	vector<vector<NationalFocus>> foci;
 };
 
