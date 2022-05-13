0f7485c11d8a5d750d8f36ecb4f29bd73767009a
pkramp <paul-niklas-kramp@web.de>
2022-05-13 12:28:58 +0200

    Refactor texture writer

--- RandomParadox/source/generic/TextureWriter.cpp
+++ RandomParadox/source/generic/TextureWriter.cpp
@@ -1,40 +1,30 @@
 #include "generic/TextureWriter.h"
 
-void TextureWriter::writeDDS(int width, int height,
+void TextureWriter::writeDDS(const int width, const int height,
                              std::vector<uint8_t> &pixelData,
-                             DXGI_FORMAT format, std::string destination) {
-  std::wstring destinationPath =
-      std::wstring(destination.begin(), destination.end());
-  Image image = Image();
-  image.width = width;
-  image.height = height;
-  image.format = format;
-  image.rowPitch = sizeof(uint8_t) * image.width * 4;
-  image.slicePitch = sizeof(uint8_t) * image.width * image.height;
-  image.pixels = pixelData.data();
-  SaveToDDSFile(image, DDS_FLAGS_NONE, destinationPath.c_str());
+                             const DXGI_FORMAT format,
+                             const std::string &path) {
+  auto wPath{std::wstring(path.begin(), path.end())};
+  Image image(width, height, format,
+              sizeof(uint8_t) * width * 4, sizeof(uint8_t) * width * height,
+              pixelData.data());
+  SaveToDDSFile(image, DDS_FLAGS_NONE, wPath.c_str());
 }
 
-void TextureWriter::writeTGA(int width, int height,
+void TextureWriter::writeTGA(const int width, const int height,
                              std::vector<uint8_t> &pixelData,
-                             std::string destination) {
-  std::wstring destinationPath =
-      std::wstring(destination.begin(), destination.end());
-  Image image = Image();
-  image.width = width;
-  image.height = height;
-  image.format = DXGI_FORMAT_B8G8R8A8_UNORM;
-  image.rowPitch = sizeof(uint8_t) * image.width * 4;
-  image.slicePitch = sizeof(uint8_t) * image.width * image.height;
-  image.pixels = pixelData.data();
-  SaveToTGAFile(image, TGA_FLAGS_NONE, destinationPath.c_str());
+                             const std::string &path) {
+  auto wPath{std::wstring(path.begin(), path.end())};
+  Image image (width, height, DXGI_FORMAT_B8G8R8A8_UNORM,
+            sizeof(uint8_t) * width * 4,
+            sizeof(uint8_t) * width * height, pixelData.data());
+  SaveToTGAFile(image, TGA_FLAGS_NONE, wPath.c_str());
 }
 
-std::vector<uint8_t> TextureWriter::readTGA(std::string destination) {
-  std::wstring destinationPath =
-      std::wstring(destination.begin(), destination.end());
+std::vector<uint8_t> TextureWriter::readTGA(const std::string &path) {
+  auto wPath{std::wstring(path.begin(), path.end())};
   ScratchImage image;
-  DirectX::LoadFromTGAFile(destinationPath.c_str(), nullptr, image);
+  DirectX::LoadFromTGAFile(wPath.c_str(), nullptr, image);
   std::vector<uint8_t> pixelData;
   for (int i = 0; i < image.GetImages()->height * image.GetImages()->width * 4;
        i++) {
