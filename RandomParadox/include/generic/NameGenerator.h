9780d09468bbfc190b7c60792c6171abd487cdb2
pkramp <paul-niklas-kramp@web.de>
2022-05-13 10:30:21 +0200

    Refactor NameGenerator.

--- RandomParadox/include/generic/NameGenerator.h
+++ RandomParadox/include/generic/NameGenerator.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 class NameGenerator {
+  using PU = ParserUtils;
   // containers
   static std::vector<std::string> nameRules;
   static std::map<std::string, std::vector<std::string>> groups;
@@ -12,16 +13,17 @@ class NameGenerator {
 public:
   // member functions
   static std::string generateName();
-  static std::string generateAdjective(std::string &name);
-  static std::string generateTag(std::string name, std::set<std::string> &tags);
-  static std::string
-  getRandomMapElement(std::string key,
-                      std::map<std::string, std::vector<std::string>> map);
-  static std::string getToken(std::vector<std::string> &rule);
+  static std::string generateAdjective(const std::string &name);
+  static std::string generateTag(const std::string name,
+                                 std::set<std::string> &tags);
+  static std::string getRandomMapElement(
+      const std::string key,
+      const std::map<std::string, std::vector<std::string>> map);
+  static std::string getToken(const std::vector<std::string> &rule);
   static std::string modifyWithIdeology(const std::string &ideology,
-                                        std::string name,
-                                        std::string adjective);
-  static void readMap(std::string path,
+                                        const std::string name,
+                                        const std::string adjective);
+  static void readMap(const std::string path,
                       std::map<std::string, std::vector<std::string>> &map);
   static void prepare();
 };
