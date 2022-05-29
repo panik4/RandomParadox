0f61f3d5a2532e9b994b891823c3364f477cf37e
pkramp <paul-niklas-kramp@web.de>
2022-05-29 23:07:07 +0200

    Resizing of DDS files, the colour maps of the base game

--- RandomParadox/include/generic/TextureWriter.h
+++ RandomParadox/include/generic/TextureWriter.h
@@ -11,5 +11,6 @@ public:
   static void writeTGA(const int width, const int height,
                        std::vector<uint8_t> &pixelData,
                        const std::string& path);
-  static std::vector<uint8_t> readTGA(const std::string& path);
+  static std::vector<uint8_t> readTGA(const std::string &path);
+  static std::vector<uint8_t> readDDS(const std::string &path);
 };
