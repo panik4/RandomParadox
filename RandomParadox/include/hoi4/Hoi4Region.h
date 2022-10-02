c9e6ae879fd69d36f38c68519ce522c5693b90f4
panik4 <25900219+panik4@users.noreply.github.com>
2022-10-02 09:38:05 +0200

    Refactor handling of hoi4 regions to make editing easier to implement

--- RandomParadox/include/hoi4/Hoi4Region.h
+++ RandomParadox/include/hoi4/Hoi4Region.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "FastWorldGenerator.h"
 #include "generic/GameRegion.h"
+#include "generic/ScenarioUtils.h"
 
 namespace Scenario::Hoi4 {
 class Region : public Scenario::Region {
+  const std::vector<std::string> buildingTypes{
+      "arms_factory",    "industrial_complex", "air_base",
+      "bunker",          "coastal_bunker",     "dockyard",
+      "naval_base",      "anti_air_building",  "synthetic_refinery",
+      "nuclear_reactor", "rocket_site",        "radar_station",
+      "fuel_silo",       "floating_harbor"};
+
 public:
   // member variables
   int armsFactories;
@@ -11,12 +19,18 @@ public:
   double development;
   int dockyards;
   int population;
-  std::map<std::string, int> resources;
   int stateCategory;
   int stratID;
+  int airport, rocketsite;
+  // Containers
+  std::vector<Scenario::Utils::Building> buildings;
+  std::map<std::string, int> resources;
   // Constructors/Destructors
   Region();
   Region(const Scenario::Region &gameRegion);
   ~Region();
+
+  // member functions
+  void calculateBuildingPositions(const Fwg::Gfx::Bitmap &heightmap);
 };
 } // namespace Scenario::Hoi4
