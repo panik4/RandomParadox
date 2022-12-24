68a878878b00b2ae83e7442efa914c3fefd746b3
panik4 <25900219+panik4@users.noreply.github.com>
2022-12-24 15:48:57 +0100

    Aoe2 experimental

---
+++ RandomParadox/include/aoe2DE/Aoe2DEParsing.h
@@ -0,0 +1,9 @@
+#include "generic/ParserUtils.h"
+#include <generic/GameProvince.h>
+#include <generic/GameRegion.h>
+
+namespace Scenario::Aoe2DE::Parsing {
+
+
+
+}; // namespace Scenario::Aoe2DE::Parsing
